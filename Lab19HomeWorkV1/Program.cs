ed0c4a1653c75c63f84885cded5fd22c2b3a4fd1
IuriiFrolov <fym95@yandex.ru>
2021-11-01 10:37:50 +0300

    Добавьте файлы проекта.

---
+++ Lab19HomeWorkV1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19HomeWorkV1
+{
+    class Computer
+    {
+        public int Id { get; set; } // код компьютера
+        public string Name { get; set; } // Марка компьютера
+        public string TypeProcessor { get; set; } // тип процессора
+        public double ProcessorFrequencyGHz { get; set; } // частота процессора
+        public double RAMCapacityGb { get; set; } // ОЗУ, объем оперативнй памяти
+        public int HardDiskCapacityGb { get; set; }// объем жесткого диска
+        public int VideoCardMemoryCapacityGb { get; set; } // объем памяти видеокарты
+        public double Cost { get; set; }// стоимость
+        public int InStock { get; set; }// количество в наличии
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {// список с 6 записями 
+            List<Computer> listComputers = new List<Computer>()
+            {
+                new Computer(){ Id=1, Name = "CityLine", TypeProcessor ="IntelCore I5", ProcessorFrequencyGHz =3.00, RAMCapacityGb=8.00, HardDiskCapacityGb= 464, VideoCardMemoryCapacityGb= 4, Cost=39000, InStock = 10 },
+                new Computer(){ Id=2, Name = "Lenovo", TypeProcessor ="IntelCore I3", ProcessorFrequencyGHz =2.00, RAMCapacityGb=6.00, HardDiskCapacityGb= 1024, VideoCardMemoryCapacityGb= 3, Cost=31000, InStock = 30 } ,
+                new Computer(){ Id=3, Name = "Asus", TypeProcessor ="IntelCore I7", ProcessorFrequencyGHz =4.00, RAMCapacityGb=16.00, HardDiskCapacityGb= 512, VideoCardMemoryCapacityGb= 3, Cost=54000, InStock = 29 },
+               new Computer(){ Id=4, Name = "CityLine", TypeProcessor ="IntelCore I3", ProcessorFrequencyGHz =2.00, RAMCapacityGb=16.00, HardDiskCapacityGb= 512, VideoCardMemoryCapacityGb= 4, Cost=60000, InStock = 8 },
+                new Computer(){ Id=5, Name = "Lenovo", TypeProcessor ="IntelCore I7", ProcessorFrequencyGHz =3.00, RAMCapacityGb=16.00, HardDiskCapacityGb= 256, VideoCardMemoryCapacityGb= 2, Cost=25000, InStock = 21 } ,
+                new Computer(){ Id=6, Name = "Asus", TypeProcessor ="IntelCore I5", ProcessorFrequencyGHz =3.00, RAMCapacityGb=8.00, HardDiskCapacityGb= 512, VideoCardMemoryCapacityGb= 3, Cost=44000, InStock = 27 },
+            };
+
+            #region все компьютеры с указанным процессором. Название процессора запросить у пользователя
+            Console.Write("Введите название процессора: ");
+            string typeProcessor = Convert.ToString(Console.ReadLine());
+            List<Computer> computers = listComputers
+               .Where(d => d.TypeProcessor == typeProcessor)
+               .ToList();
+            foreach (var d in computers)
+                Console.WriteLine($" № {d.Id}, Производитель: {d.Name},Тип процессора: {d.TypeProcessor} ");
+            Console.WriteLine();
+            #endregion
+
+            #region все компьютеры с объемом ОЗУ не ниже, чем указано. Объем ОЗУ запросить у пользователя;
+            Console.Write("Введите минимальный объем (Гб) оперативной памяти : ");
+            int ramCapacityGb = Convert.ToInt32(Console.ReadLine());
+            List<Computer> computers1 = listComputers
+               .Where(d => d.RAMCapacityGb >= ramCapacityGb)
+               .ToList();
+            foreach (var d in computers1)
+                Console.WriteLine($" № {d.Id}, Производитель: {d.Name}, Объем ОЗУ: {d.RAMCapacityGb} ");
+            Console.WriteLine();
+            Console.ReadKey();
+            #endregion
+
+            #region вывести весь список, отсортированный по увеличению стоимости;
+
+            Console.WriteLine("Выводим весь список, отсортированный по увеличению стоимости: ");
+            List<Computer> computers2 = (from d in listComputers
+                                         orderby d.Cost
+                                         select d).ToList();
+            foreach (var d in computers2)
+                Console.WriteLine($" № {d.Id}, Производитель: {d.Name}, Стоимость: {d.Cost} ");
+            Console.WriteLine();
+            Console.ReadKey();
+            #endregion
+
+            #region вывести весь список, сгруппированный по типу процессора;
+            Console.WriteLine("Выводим весь список, сгруппированный по типу процессора: ");
+            List<Computer> computers3 = (from d in listComputers
+                                         orderby d.TypeProcessor, d.Id
+                                         select d).ToList();
+            foreach (var d in computers3)
+                Console.WriteLine($"Тип процессора: {d.TypeProcessor} № {d.Id}, Производитель: {d.Name}, Стоимость: {d.Cost} ");
+            Console.WriteLine();
+            Console.ReadKey();
+            #endregion
+
+            #region найти самый дорогой и самый бюджетный компьютер;
+            var computers4 = listComputers
+                .Max(d => d.Cost);
+            Console.WriteLine($"Самый дорогой компьютер стоит: {computers4}");
+            var computers5 = listComputers
+                .Min(d => d.Cost);
+            Console.WriteLine($"Самый бюджетный компьютер стоит: {computers5}");
+            Console.WriteLine();
+            Console.ReadKey();
+            #endregion
+
+            #region есть ли хотя бы один компьютер в количестве не менее 30 штук?
+            Console.WriteLine("Есть ли хотя бы один компьютер в количестве не менее 30 штук? ");
+                        var computers6 = listComputers
+                .Any(d => d.InStock >= 30);
+            if (true)
+            {
+                Console.WriteLine("Да, есть.");
+            }
+            else { Console.WriteLine("Нет, нету."); }
+            #endregion
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Simple19/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19Lesson19
+{
+    class Door
+    {
+        public int Id { get; set; }
+        public int Width { get; set; }
+        public int Hight { get; set; }
+        public int Cost { get; set; }
+        public string Manufacturer { get; set; }
+        public string Material { get; set; }
+        public List<string> Composition { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Door> listDoor = new List<Door>()
+            {
+                new Door(){ Id=1, Width=1000, Hight=2000, Cost= 100000, Manufacturer="Заря",Material="Металл", Composition=new List<string>(){"Полотно","Коробка","Ручка","Петли","Глазок","Замок" } },
+                new Door(){ Id=2, Width=1000, Hight=2000, Cost= 700, Manufacturer="Москва",Material="Дерево", Composition=new List<string>(){"Полотно","Коробка","Ручка","Петли","Глазок","Замок" } },
+                new Door(){ Id=3, Width=1000, Hight=2000, Cost= 7200, Manufacturer="Билд",Material="Пластик", Composition=new List<string>(){"Полотно","Коробка","Ручка","Петли","Глазок","Замок" } },
+                new Door(){ Id=4, Width=1000, Hight=2000, Cost= 1500, Manufacturer="Ростов",Material="Дерево", Composition=new List<string>(){"Полотно","Коробка","Ручка","Петли","Глазок","Замок" } },
+                new Door(){ Id=5, Width=1000, Hight=2000, Cost= 900, Manufacturer="Казань",Material="Металл", Composition=new List<string>(){"Полотно","Коробка","Ручка","Петли","Глазок","Замок" } },
+                new Door(){ Id=6, Width=1000, Hight=2000, Cost= 3000, Manufacturer="Билд",Material="Пластик", Composition=new List<string>(){"Полотно","Коробка","Ручка","Петли","Глазок","Замок" } },
+                new Door(){ Id=7, Width=1000, Hight=2000, Cost= 1700, Manufacturer="Заря",Material="Металл", Composition=new List<string>(){"Полотно","Коробка","Ручка","Петли","Глазок","Замок" } },
+                new Door(){ Id=8, Width=1000, Hight=2000, Cost= 15000, Manufacturer="Заря",Material="Дерево", Composition=new List<string>(){"Полотно","Коробка","Ручка","Петли","Глазок","Замок" } },
+                new Door(){ Id=9, Width=1000, Hight=2000, Cost= 18000, Manufacturer="Заря",Material="Металл", Composition=new List<string>(){"Полотно","Коробка","Ручка","Петли","Глазок","Замок" } },
+                new Door(){ Id=10, Width=1000, Hight=2000, Cost= 3000, Manufacturer="Заря",Material="Дерево" },
+
+            };
+            //List<Door> doors = (from d in listDoor
+            //                    orderby d.Material , d.Id
+            //                    select d).ToList();
+
+            var doors = listDoor
+                .Any(d => d.Cost >= 1000);
+
+            Console.WriteLine("Есть ли боьше 30 ? {0}", doors);
+
+
+
+
+            //foreach (var d in doors)
+            //    Console.WriteLine($"{d.Id}, {d.Width}, {d.Hight}, {d.Cost}, {d.Manufacturer}, {d.Material}");
+            Console.ReadKey();
+
+
+            #region HomeWork
+            /* Console.Write("Введите название процессора: ");
+             string t = Convert.ToString (Console.ReadLine());
+             List<Door> doors = listDoor.Where(d => d.Manufacturer == t).ToList();*/
+            /*Console.Write("Введите минимаьный объем ОЗУ ");
+            int t = Convert.ToInt32(Console.ReadLine());
+            List<Door> doors = listDoor.Where(d => d.Cost >= t).ToList();*/
+
+
+            /*List<Door> doors = (from d in listDoor
+                                orderby d.Cost
+                                select d).ToList();*/
+            //Это для типа сгруппированного
+            //List<Door> doors = (from d in listDoor
+            //                    orderby d.Material, d.Id
+            //                    select d).ToList();
+            ////Это для Foreach
+            //Foreach (var d in doors)
+            //Console.WriteLine($"{d.Id}, {d.Width}, {d.Hight}, {d.Cost}, {d.Manufacturer}, {d.Material}");
+
+            //var doors = listDoor
+            //    .Max(d => d.Cost);
+            //Console.WriteLine("Max = {0}", doors);
+            //var doors = listDoor
+            //    .Min(d => d.Cost);
+            //Console.WriteLine("Min = {0}", doors);
+            // Последний вопрос!
+            //var doors = listDoor
+            //    .Any(d => d.Cost >= 1000);
+
+            //Console.WriteLine("Есть ли боьше 30 ? {0}", doors);
+
+
+
+            //List<Door> doors = listDoor.Max(d => d.Cost).ToList(); не работает
+
+            #endregion
+        }
+    }
+}
+
